405ca535d0bff5669b6a10921a41e8525df8d8a1
Riley Anderson <scout1553@gmail.com>
2018-09-10 17:14:06 -0600

    made a character script and added MoveSpeed/JumpHight

---
+++ DGM1610_Game/Assets/Scrips/CharaterMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharaterMove : MonoBehaviour 
+{
+
+	public int MoveSpeed;
+	public float JumpHeight;
+
+	// Use this for initialization
+	void Start () 
+	{
+		print("Hello World!");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	}
+}
