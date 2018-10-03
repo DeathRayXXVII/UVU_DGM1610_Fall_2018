29ba8c4d991973ece4bf9c6b0e44697cf9afa479
Riley Anderson <scout1553@gmail.com>
2018-10-03 17:16:29 -0600

    Started the Level manager script and changed some of the background

---
+++ DGM1610_Game/Assets/Scrips/LevelManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour 
+{
+	public GameObject CurrentCheakPoint;
+	private Rigidbody2D player;
+	// Particles
+	public GameObject DeathParticles;
+	public GameObject RespawnParticle;
+	// Respawn Delay
+	public float RespawnDelay;
+	// Point Penalty on Death
+	public int PointPenaltyOnDeath;
+	// Store Gravity Value
+	private float GravityStore;
+
+	// Use this for initialization
+	void Start () 
+	{
+		player = FindObjectOfType<Rigidbody2D> ();		
+	}
+	
+	public void RespawnPlayer()
+	{
+		StartCoroutine ("RespawnPlayerCo");
+	}
+	public IEnumerator RespawnPlayerCo()
+	{
+		//Generate Death Particale
+		Instantiate (DeathParticles, player.transform.position, player.transform.rotation);
+		//Hide Player
+		player.enabled = false;
+		player.GetComponent<Renderer>().enabled = false;
+		//Gravity Reset
+		GravityStore = player.GetComponent<Rigidbody2D>().gravityScale;
+		player.GetComponent<Rigidbody2D>().gravityScale = 0f;
+	}
+
+}
---
+++ DGM1610_Game/Assets/Scrips/Quiz_2.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quiz_2 : MonoBehaviour 
+{
+	public int num1;
+    public int num2;
+    public int total;
+
+    void Start()
+    {
+        Calculat();
+    }
+    public void Calculat()
+    {
+        Add(num1,num2);
+    }
+
+    public void Add(int One, int Two)
+    {
+        total = One + Two;
+        print(total);
+    }
+}
