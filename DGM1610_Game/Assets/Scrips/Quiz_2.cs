bf1d70df786f158bf5277a456ad996b279ffc8d1
Riley Anderson <scout1553@gmail.com>
2018-10-08 15:44:41 -0600

    quiz 2 homework

--- DGM1610_Game/Assets/Scrips/Quiz_2.cs
+++ DGM1610_Game/Assets/Scrips/Quiz_2.cs
@@ -7,6 +7,7 @@ public class Quiz_2 : MonoBehaviour
 	public int num1;
     public int num2;
     public int total;
+	int total2;
 
     void Start()
     {
@@ -15,11 +16,17 @@ public class Quiz_2 : MonoBehaviour
     public void Calculat()
     {
         Add(num1,num2);
+		Subtract (num1, num2);
     }
 
     public void Add(int One, int Two)
     {
         total = One + Two;
-        print(total);
+		print(total);
     }
+	public void Subtract(int One, int Two)
+	{
+		total2 = One - Two;
+		print (total2);
+	}
 }
