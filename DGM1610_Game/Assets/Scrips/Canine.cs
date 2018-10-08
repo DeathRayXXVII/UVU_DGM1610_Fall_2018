b6eb133cc1eb17af52a8de6cb152ade0dd05bb58
Riley Anderson <scout1553@gmail.com>
2018-10-08 17:17:25 -0600

    finished LevelManager script and some level changes

---
+++ DGM1610_Game/Assets/Scrips/Canine.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Canine : Animal
+{
+	public bool tail;
+	public bool bark;
+	public Color coatColor;
+	public int size;
+	public string earType;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- DGM1610_Game/Assets/Scrips/LevelManager.cs
+++ DGM1610_Game/Assets/Scrips/LevelManager.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour 
 {
+	
 	public GameObject CurrentCheakPoint;
-	private Rigidbody2D player;
+	public Rigidbody2D Player;
 	// Particles
 	public GameObject DeathParticles;
 	public GameObject RespawnParticle;
@@ -19,7 +20,7 @@ public class LevelManager : MonoBehaviour
 	// Use this for initialization
 	void Start () 
 	{
-		player = FindObjectOfType<Rigidbody2D> ();		
+		//player = FindObjectOfType<Rigidbody2D> ();		
 	}
 	
 	public void RespawnPlayer()
@@ -28,16 +29,29 @@ public class LevelManager : MonoBehaviour
 	}
 	public IEnumerator RespawnPlayerCo()
 	{
-		//Generate Death Particale
-		Instantiate (DeathParticles, player.transform.position, player.transform.rotation);
-		//Hide Player
-		player.enabled = false;
-		player.GetComponent<Renderer>().enabled = false;
-		//Gravity Reset
-		GravityStore = player.GetComponent<Rigidbody2D>().gravityScale;
-		player.GetComponent<Rigidbody2D>().gravityScale = 0f;
-		player.GetComponent<Rigidbody2D>().velocity = vector2.zero;
+		// Generate Death Particale
+		Instantiate (DeathParticles, Player.transform.position, Player.transform.rotation);
+		// Hide Player
+		//Player.enabled = false;
+		Player.GetComponent<Renderer>().enabled = false;
+		// Gravity Reset
+		GravityStore = Player.GetComponent<Rigidbody2D>().gravityScale;
+		Player.GetComponent<Rigidbody2D>().gravityScale = 0f;
+		Player.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
 		// Point penalty
+		ScoreManager.AddPoints(-PointPenaltyOnDeath);
+		// Debug Message
+		Debug.Log ("Player Respawn");
+		// Respawn Delay
+		yield return new WaitForSeconds (RespawnDelay);
+		// Gravity Restore
+		Player.GetComponent<Rigidbody2D>().gravityScale = GravityStore;
+		// Match Players transform position
+		Player.transform.position = CurrentCheakPoint.transform.position;
+		// Show Player
+		//Player.enabled = true;
+		Player.GetComponent<Renderer>().enabled = true;
+		// Spawn Particle
+		Instantiate (RespawnParticle, CurrentCheakPoint.transform.position, CurrentCheakPoint.transform.rotation);
 	}
-
 }
