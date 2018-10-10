4c93be003b60f45b9d1dd4aafb5eaa42380606f8
Riley Anderson <scout1553@gmail.com>
2018-10-10 15:49:09 -0600

    changed some of the sprites and an animation to the green guy, homework

---
+++ DGM1610_Game/Assets/Scrips/homwork1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class homwork1 : Animal {
+
+	public Color skinColor;
+	public int size;
+	public string voice;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ DGM1610_Game/Assets/Scrips/homwork1_1.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class homwork1_1 : homwork1 {
+
+	public bool frendly;
+	public bool wepon;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
