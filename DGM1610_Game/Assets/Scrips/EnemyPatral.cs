e4ca6763d7a621a1c26f99c50ea70d3dee7559d0
Riley Anderson <scout1553@gmail.com>
2018-10-15 16:13:20 -0600

    homwork

---
+++ DGM1610_Game/Assets/Scrips/EnemyPatral.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatral : MonoBehaviour 
+{
+	// Movement Variables
+	public float MoveSpeed;
+	public bool MoveRight;
+
+	// Wall Check
+	public Transform WallCheck;
+	public floatWall CheckRadius;
+	public LayerMask WhatIsWall;
+	private bool HittingWall;
+
+	// Edge Check
+	private bool NotAtEdge;
+	public Transform EdgeCheck;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ DGM1610_Game/Assets/Scrips/School Crap/Homwork2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Homwork2 : MonoBehaviour 
+{
+
+	private string spectral;
+
+	// Use this for initialization
+	void Start () 
+	{
+		if(spectral == "Wakes Up")
+			print ("Good morning" = spectral);
+		else if (spectral == "Sleeping")
+			pring ("Good Night" + spectral);
+		else if (spectral == "Zoning")
+			pirnt (spectral + " is Day Dreaming");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
