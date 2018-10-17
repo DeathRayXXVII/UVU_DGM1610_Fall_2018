99f69be5db495a74e4476adac32f3dc2b0b67ae2
Riley Anderson <scout1553@gmail.com>
2018-10-17 17:17:53 -0600

    added a main menu and working on a roling patrol script

---
+++ DGM1610_Game/Assets/Scrips/RollingPatral.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollingPatral : EnemyPatral {
+
+	// Use this for initialization
+	 public Texture2D tex;
+
+    private Rigidbody2D rb2D;
+    private Sprite mySprite;
+    private SpriteRenderer sr;
+    private float revSpeed = 50.0f;
+
+    void Awake()
+    {
+        sr = gameObject.AddComponent<SpriteRenderer>();
+        rb2D = gameObject.AddComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        mySprite = Sprite.Create(tex, new Rect(0.0f, 0.0f, tex.width, tex.height), new Vector2(0.5f, 0.5f), 100.0f);
+        transform.localScale = new Vector3(3.0f, 3.0f, 3.0f);
+        rb2D.gravityScale = 0.0f;
+        sr.sprite = mySprite;
+    }
+
+    void FixedUpdate()
+    {
+        rb2D.MoveRotation(rb2D.rotation + revSpeed * Time.fixedDeltaTime);
+    }
+}
