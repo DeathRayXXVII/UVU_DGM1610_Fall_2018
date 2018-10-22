a119a73f7a93d06208bcd459ca55884bfe995e8f
Riley Anderson <scout1553@gmail.com>
2018-10-22 17:18:43 -0600

    added a projectile to kill enemy

---
+++ DGM1610_Game/Assets/Scrips/PlayerShoot.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour {
+
+	//Shoot variables
+	public Transform FirePoint;
+	public GameObject projectile;
+
+	// Use this for initialization
+	void Start () {
+		projectile = GameObject.Find("FireBall");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.RightControl))
+			Instantiate(projectile,FirePoint.position, FirePoint.rotation);
+	}
+}
