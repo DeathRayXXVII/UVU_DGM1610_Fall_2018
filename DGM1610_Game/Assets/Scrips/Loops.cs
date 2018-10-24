168f908178b4ab6bea07d6a23af750e5e044757e
Riley Anderson <scout1553@gmail.com>
2018-10-24 16:39:43 -0600

    Working on some loops

---
+++ DGM1610_Game/Assets/Scrips/Loops.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loops : MonoBehaviour 
+{
+	public int num = 40;
+
+	// Use this for initialization
+	void Start () 
+	{
+		// for(int i = 0; i <= 100;  i++ )
+		// {
+		// 	print(i + "Ants Marching");
+		// }
+		while(num > 0)
+		{
+			print ("Countdown"num);
+			num --;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
