b90fc460465d661a8f300c4c9e78a0784f9c7259
Riley Anderson <scout1553@gmail.com>
2018-10-29 17:17:26 -0600

    Did some Swithes data work

---
+++ DGM1610_Game/Assets/Scrips/School Crap/Switches.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchesH : MonoBehaviour 
+{
+	public int num;
+	public string name;
+
+	// Use this for initialization
+	void Start () 
+	{
+		switch(num == 1)
+		{
+			case 1:
+				print("You have won " + num + "cat");
+			break; 
+			case 2:
+				print("You have won " + num + "tacos");
+			break; 
+			case 3:
+				print("You have won " + num + "rats");
+			break; 
+			default:
+				print("Next time :(");
+			break
+		}
+		switch(name)
+		{
+			case :
+			break;
+			case :
+			break;
+			case :
+			break;
+			case :
+			break;
+			default:
+			break;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
---
+++ DGM1610_Game/Assets/Scrips/Switches.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switches : MonoBehaviour 
+{
+	public int num;
+	public string name;
+
+	// Use this for initialization
+	void Start () 
+	{
+		/*if(num == 1)
+			print("Hurray you picked the lucky number one!");
+		else
+			print("Better luck next time :(");
+	
+		switch(num)
+		{
+			case 1:
+				print("Hurray you picked" + num);
+			break;
+			case 3:
+				print("Hurray you picked" + num);
+			break;
+			case 6:
+				print("Hurray you picked" + num);
+			break;
+			case 10:
+				print("Hurray you picked" + num);
+			break;
+			default:
+				print("I dont understand" + num);
+			break;
+		} 
+		*/
+		switch (name){
+			case "Jason":
+				print("Welcome to camp crystal lake " + name + "- Love Mother");
+			break;
+			case "Michael":
+				print("Welcome to Haddonfield, Ill " + name);
+			break;
+			case "Freddie":
+				print("Welcome to Elm Street " + name);
+			break;
+			case "Leatherface":
+				print("The stars and stripes are big at night...Deep in the heart of Texas");
+			break;
+			default:
+				print("I dont know " + name);
+			break;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
