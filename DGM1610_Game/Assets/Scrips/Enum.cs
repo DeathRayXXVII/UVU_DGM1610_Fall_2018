b47ad8325e645bc4128ae8f9d477bf2acd53dd4e
Riley Anderson <scout1553@gmail.com>
2018-11-07 16:46:14 -0700

    Enum obsolete

---
+++ DGM1610_Game/Assets/Scrips/Enum.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enum : MonoBehaviour 
+{
+		enum Inventory {Cheese, Meat , Sword, Shield, Map, Bow, Arrows, Backpack};
+		
+	// Use this for initialization
+	void Start () 
+	{
+		print("Cheese" + Inventory.Cheese);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ DGM1610_Game/Assets/Scrips/School Crap/ArrayQuiz.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrayQuiz : MonoBehaviour 
+{
+	public string[] PetOwner;
+	public int hp;
+
+	// Use this for initialization
+	void Start () 
+	{
+		PetOwner = new string [6];
+		PetOwner[0] = "Cat";
+		PetOwner[1] = "Dog";
+		PetOwner[2] = "Parite";
+		PetOwner[3] = "Lizard";
+		PetOwner[4] = "Sip";
+		PetOwner[5] = "Goldfish";
+		print(PetOwner[3]);
+
+		//foreach(hp)
+		
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
