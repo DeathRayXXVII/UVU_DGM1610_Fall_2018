768c93e4e6f43352dc5068409b05bd2ecea6d49d
Riley Anderson <scout1553@gmail.com>
2018-11-28 17:19:39 -0700

    made a winning

--- DGM1610_Game/Assets/Scrips/Winning.cs
+++ DGM1610_Game/Assets/Scrips/Winning.cs
@@ -1,42 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.UI;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ScoreManager : MonoBehaviour 
-{
-	public static int Score;
-	public GameObject WinScore;
-
-	Text ScoreText;
+public class Winning: MonoBehaviour {
 	public Text WinText;
 
-	void Awake()
-	{
-		Time.timeScale = 1;
-	}
-		
 	// Use this for initialization
 	void Start () 
 	{
-		ScoreText = GetComponent<Text>();
-
-		Score = 0;
-
-		WinText.GetComponent<Text>().enabled = false;
+		
 	}
 	
 	// Update is called once per frame
-	void Update () 
+	void Update ()
 	{
-		if(Score < 0)
-		{
-			Score = 0;
-		}
-		ScoreText.text = " " + Score;
-		
-		
 		//if player hits the Escape key return to start menu
 		if(Input.GetKeyDown(KeyCode.Escape))
 		{
@@ -48,15 +26,11 @@ public class ScoreManager : MonoBehaviour
 		//if the plalyer win display win text
 		if(Other.tag == "Player")
 		{
-			print("Win Score Reached = " + Score);
+			print("Win Score Reached = ");
 			WinText.GetComponent<Text>().enabled = true;
 			Time.timeScale = 0;
 		}
 		
-	}
-	public static void AddPoints (int PointsToAdd)
-	{
-		Score += PointsToAdd;
-		//Score = Score + PointsToAdd;
+		
 	}
 }
